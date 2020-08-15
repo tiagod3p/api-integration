0feb0eb91267e545483174e1bac922490cc7506f
BMS <saymon.titi@gmail.com>
2020-08-15 12:58:09 -0300

    feat: ensure express use json and urlencoded

--- src/server.js
+++ src/server.js
@@ -3,6 +3,10 @@ const cors = require('cors')
 
 const server = express()
 
+server.use(cors())
+server.use(express.json())
+server.use(express.urlencoded({extended: true}))
+
 server.listen(3000, () => {
     console.log("server online...")
 })
