15a8cbd6f6ea997c349ba984fa5e703a2b29d2a6
BMS <saymon.titi@gmail.com>
2020-08-15 14:02:18 -0300

    feat: ensure bling service returns nothing if an error occurred

--- src/services/bling.js
+++ src/services/bling.js
@@ -5,27 +5,32 @@ const key = process.env.BLING_KEY
 
 module.exports = {
     async createOrder(deal) {
-        const xml = `
-        <?xml version="1.0" encoding="UTF-8"?>
-        <pedido>
-         <cliente>
-         <nome>${deal.person_id === null ?  "Any name" : deal.person_id.name}</nome>
-         </cliente>
-         
-         <volume>
-         <servico> ANY VOLUME SERVICE </servico>
-         </volume>
-
-         <itens>
-         <item>
-         <codigo>${deal.id}</codigo>
-         <descricao> ANY ITEM DESCRIPTION </descricao>
-         <qtde>${deal.products_count}</qtde>
-         <vlr_unit>${deal.value/deal.products_count}</vlr_unit>
-         </item>
-         </itens>
-        </pedido>
-        `
-        await axios.post(`${baseUrl}/pedido/json?apikey=${key}&xml=${xml}`)
+        try {
+            const xml = `
+            <?xml version="1.0" encoding="UTF-8"?>
+            <pedido>
+             <cliente>
+             <nome>${deal.person_id === null ?  "Any name" : deal.person_id.name}</nome>
+             </cliente>
+             
+             <volume>
+             <servico> ANY VOLUME SERVICE </servico>
+             </volume>
+    
+             <itens>
+             <item>
+             <codigo>${deal.id}</codigo>
+             <descricao> ANY ITEM DESCRIPTION </descricao>
+             <qtde>${deal.products_count}</qtde>
+             <vlr_unit>${deal.value/deal.products_count}</vlr_unit>
+             </item>
+             </itens>
+            </pedido>
+            `
+            await axios.post(`${baseUrl}/pedido/json?apikey=${key}&xml=${xml}`)
+        } catch(err) {
+            console.error(err)
+            return
+        }
     }
 }
