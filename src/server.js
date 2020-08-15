b33327b2b5a6e3dab9c20cfc5a1b0dcccddadf0b
BMS <saymon.titi@gmail.com>
2020-08-15 13:14:56 -0300

    chore: ensure mongoose connect on mongodb atlas

--- src/server.js
+++ src/server.js
@@ -1,7 +1,20 @@
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
 
 const server = express()
+
+const name = process.env.DB_NAME
+const pass = process.env.DB_PASSWORD
+
+mongoose.connect(`mongodb+srv://developer:${pass}@development.1hu5y.mongodb.net/${name}?retryWrites=true&w=majority`, { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Connection succeed")
+}).catch((err) => {
+    console.log(err)
+})
 
 server.use(cors())
 server.use(express.json())
