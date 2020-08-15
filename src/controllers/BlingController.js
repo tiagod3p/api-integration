c4426d6d47d393247d77b7a427d7bf73fce1742c
BMS <saymon.titi@gmail.com>
2020-08-15 13:55:19 -0300

    feat: ensure BlingController returns 500 if an unexpected server error occurred

--- src/controllers/BlingController.js
+++ src/controllers/BlingController.js
@@ -2,12 +2,20 @@ const Deal = require("../models/Deal")
 
 module.exports = {
     async index(req, res) {
-        const deals = await Deal.find({})
+        try {
+            const deals = await Deal.find({})
 
-        if (!deals) return res.status(404).json({
-            message: "No deals with status won on database colletions yet!"
-        })
-
-        return res.json(deals)
+            if (!deals) return res.status(404).json({
+                message: "No deals with status won on database colletions yet!"
+            })
+    
+            return res.json(deals)
+        } catch(err) {
+            console.error(err)
+            return res.status(500).json({
+                message: "An error occurred! Please try again later.",
+                err
+            })
+        }
     }
 }
