de89f86868e4bbe576a15e72626e4d72d5f40a13
BMS <saymon.titi@gmail.com>
2020-08-15 13:06:09 -0300

    feat: create a fuction to get deals with status won on pipedrive

---
+++ src/services/pipedrive.js
@@ -0,0 +1,14 @@
+const axios = require('axios')
+
+const baseUrl = process.env.PIPEDRIVE_BASE_URL
+const key = process.env.PIPEDRIVE_KEY
+
+module.exports = {
+    async getDealsWithStatusWon() {
+        const { data } = await axios.get(`${baseUrl}/deals?limit=500&api_token=${key}`)
+        
+        const dealsWithStatusWon = data.data.filter(data => data.status === "won")
+
+        return dealsWithStatusWon
+    }
+}
