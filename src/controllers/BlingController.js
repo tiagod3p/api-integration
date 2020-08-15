3060a5a69d679261cb269c4a66b36530e18bde36
BMS <saymon.titi@gmail.com>
2020-08-15 13:53:08 -0300

    feat: ensure BlingController returns 404 if don't exists deals with status won on database

--- src/controllers/BlingController.js
+++ src/controllers/BlingController.js
@@ -4,6 +4,10 @@ module.exports = {
     async index(req, res) {
         const deals = await Deal.find({})
 
+        if (!deals) return res.status(404).json({
+            message: "No deals with status won on database colletions yet!"
+        })
+
         return res.json(deals)
     }
 }
