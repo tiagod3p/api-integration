5326b3f945077203ddef4950627a430fd0e0a940
BMS <saymon.titi@gmail.com>
2020-08-15 13:46:49 -0300

    feat: ensure PipeDriveController add deals to database if don't exists in database yet

--- src/controllers/PipeDriveController.js
+++ src/controllers/PipeDriveController.js
@@ -1,6 +1,8 @@
 const { getDealsWithStatusWon } = require("../services/pipedrive")
 const { createOrder } = require("../services/bling")
 
+const Deal = require("../models/Deal")
+
 module.exports = {
     async index(req, res) {
         try {  
@@ -11,6 +13,34 @@ module.exports = {
             })
 
             const createOrderPromises = wonDeals.map(deal => {
+                const id = deal.id
+                const personName = deal.person_id === null ? "Any Name" : deal.person_id.name
+                const orgName = deal.org_id === null ? "Any Organization" : deal.org_id.name
+                const dealTitle = deal.title === null ? "Any Title" : deal.title
+                const dealValue = deal.value === null ? "Any Value" : deal.value
+                const createdAt = deal.add_time
+                const updatedAt = deal.update_time
+
+                Deal.findOne({"id": id}, "id", async (err, deal) => {
+                    if (err) return res.status(500).json({
+                        message: "An internal error occurred! Please try again later."
+                    })
+
+                    if (deal === null) {
+                        const newDeal = new Deal({
+                            id,
+                            person_name: personName,
+                            org_name: orgName,
+                            deal_title: dealTitle,
+                            deal_value: dealValue,
+                            created_at: createdAt,
+                            updated_at: updatedAt
+                        })
+                
+                        await newDeal.save()
+                    }
+                })
+                
                 return createOrder(deal)
             })
     
@@ -21,7 +51,7 @@ module.exports = {
           } catch(err) {
               console.error(err)
               return res.status(500).json({
-                  message: "An error ocurred! Please try again later.",
+                  message: "An error occurred! Please try again later.",
                   err
               })
           }
